8f4bcce86906b3b1f8cb5fecf4dc4d91209f6084
Sahil Gupta <155609546+2008sahil@users.noreply.github.com>
2024-05-12 12:31:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn CICD USING GITHUB ACTIONS
         </a>
       </header>
     </div>
